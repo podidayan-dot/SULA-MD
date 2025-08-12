7d386908d9a81787765f12215eaba06650f49221
podidayan-dot <podidayan@gmail.com>
2025-08-12 17:19:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,7 +12,7 @@ AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS === undefined ?"true" : process
 MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
 AUTO_REPLY: process.env.AUTO_REPLY === undefined ? "false" : process.env.AUTO_REPLY,
-AI_REPLAY: process.env.AI_REPLAY === undefined ? "false" : process.env.AI_REPLAY,//AI_REPLAY KARANA KOTASA ADD KARA
+AI_REPLAY: process.env.AI_REPLAY === undefined ? "true" : process.env.AI_REPLAY,//AI_REPLAY KARANA KOTASA ADD KARA
 AUTO_STICKER: process.env.AUTO_STICKER === undefined ? "false" : process.env.AUTO_STICKER,
 ANTI_BAD: process.env.ANTI_BAD === undefined ? "false" : process.env.ANTI_BAD,
 ANTI_LINK: process.env.ANTI_LINK === undefined ? "false" : process.env.ANTI_LINK,
